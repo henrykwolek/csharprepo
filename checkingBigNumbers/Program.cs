987de2f7ed40d13782c6d63f55851ac9e5e5b617
henrykwolek <henrykwolek15@gmail.com>
2021-01-31 21:40:20 +0100

    Added new project

---
+++ checkingBigNumbers/Program.cs
@@ -0,0 +1,63 @@
+﻿//Autor: Henryk Wołek IIC
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace checkingBigNumbers
+{
+    class Program
+    {
+        //Metoda która zamienia daną wartość wejściową string na strukturę BigInteger w systemie dziesiętnym
+        static string binaryToDecimal(string InValue)
+        {
+            BigInteger result = 0;
+            foreach(char digit in InValue)
+            {
+                result <<= 1;
+                result += digit == '1' ? 1 : 0;
+            }
+            //Zwraca wartość string
+            return result.ToString();
+        }
+        static void Main(string[] args)
+        {
+            string line;
+            //Deklaracja list
+            List<string> stringNumbersBeforeConversion = new List<string>();
+            List<BigInteger> extremelyLargeNumbers = new List<BigInteger>();
+            BigInteger bigNumber = 0;
+
+            //Czytanie pliku liczby.txt
+            System.IO.StreamReader fileRead = new System.IO.StreamReader(@"C:\Users\henry\Desktop\liczby.txt");
+            while((line = fileRead.ReadLine()) != null)
+            {
+                //Na początku program dopisuje do listy każdą liczbę z każdego wiersza jako string
+                stringNumbersBeforeConversion.Add(line);
+            }
+
+            for (int i = 0; i < stringNumbersBeforeConversion.Count; i++)
+            {
+                //Gdy poprzednia lista jest zapełniona, ta pętla konwertuje elementy na liczby w systemie dziesiętnym
+                bigNumber = BigInteger.Parse(binaryToDecimal(stringNumbersBeforeConversion[i]));
+                extremelyLargeNumbers.Add(bigNumber);
+            }
+
+            //Zapisanie listy jako tablicy
+            BigInteger[] bigIntArr = new BigInteger[1000];
+            bigIntArr = extremelyLargeNumbers.ToArray();
+
+            for (int j = 0; j < bigIntArr.Length; j++)
+            {
+                //Jeśli element w tablicy jest równy największemu elementowi z listy, pętla zakończy się
+                if (bigIntArr[j] == extremelyLargeNumbers.Max())
+                {
+                    //Wyświetlenie wiersza na którym znajduje się dana liczba
+                    Console.WriteLine("Największa liczba znajduje się na wierszu {0}", (j + 1).ToString());
+                    break; 
+                }           
+            }
+        }
+    }
+}
